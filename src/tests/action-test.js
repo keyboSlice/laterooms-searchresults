ad1aa90670657dd45b1749fb401f45b275cca4b6
Keir Lavelle <keir@Jamess-MacBook-Pro.local>
2017-07-20 09:47:10 +0100

    added action test for setting minimum star filter

---
+++ src/tests/action-test.js
@@ -0,0 +1,15 @@
+import test from "ava";
+import * as actions from "../assets/js/actions.js";
+import * as types from "../assets/js/action-types.js";
+
+test ("Create an action to set the minimum star filter", t => {
+
+    const stars = 2;
+
+    const expected = {
+        type: types.SET_MINIMUM_STAR_FILTER,
+        stars
+    };
+
+    t.is (actions.setMinimumStarFilter (stars), expected);
+});
