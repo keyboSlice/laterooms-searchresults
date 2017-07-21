d5dd1701900b807dbe2e7d398c75614435adaab4
Keir Lavelle <keir@Jamess-MacBook-Pro.local>
2017-07-21 02:43:17 +0100

    added null state for results

--- src/assets/js/components/results.js
+++ src/assets/js/components/results.js
@@ -4,7 +4,31 @@ import { connect } from "react-redux";
 const mapStateToProps = state => {
 
     return {
-        hotels: []
+        hotels: state.get ("hotels").toJS ().filter (h => {
+
+            let valid = true;
+            let minimumStarRating = state.getIn (["filters", "minimumStarRating"]);
+            let maximumStarRating = state.getIn (["filters", "maximumStarRating"]);
+            let facilities = state.getIn (["filters", "facilities"]).toJS ();
+
+            if (maximumStarRating > 0) {
+
+                if (h.StarRating <= minimumStarRating || h.StarRating > maximumStarRating) {
+
+                    valid = false;
+                }
+            }
+
+            if (facilities.length && ! h.Facilities.some (f => facilities.indexOf (f) >= 0)) {
+
+                valid = false;
+            }
+
+            return valid;
+        }),
+
+        minimumStarRating: state.getIn (["filters", "minimumStarRating"]),
+        maximumStarRating: state.getIn (["filters", "maximumStarRating"])      
     };
 };
 
@@ -15,11 +39,34 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     };
 };
 
-const Results = () => (
+class Results extends React.Component {
 
-    <div className="page__component results">
+    render () {
 
-    </div>
-);
+        let {
+            minimumStarRating,
+            maximumStarRating,
+            hotels
+        } = this.props;
+
+        let resultsContent = hotels.length 
+            ? "foo"
+            : <div className="results__no-results page__component">
+                <h1 className="results__no-results-title">
+                    <i className="fa fa-frown-o"></i>
+                    Sorry!<br />No results matched your search
+                </h1>
+            </div>
+
+        return (
+
+            <div className="results">
+
+                { resultsContent }
+
+            </div>
+        )
+    }
+}
 
 export default connect (mapStateToProps) (Results);
